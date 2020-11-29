c83642056f66bb4f0053e142339bead2fd1226f4
sun <760172827@qq.com>
2020-11-29 23:09:11 +0800

    异步加载数据

--- react-knowledge-study/src/hooks/UseEffect.jsx
+++ react-knowledge-study/src/hooks/UseEffect.jsx
@@ -1,7 +1,16 @@
 import React, { useState, useEffect } from "react";
 
-function UseEffect (props) {
-  let [data, setData] = useState({ count: 0 });
+function UseEffect(props) {
+  let [data, setData] = useState({ count: 0, name: "zhang" });
+
+  function loadData() {
+    return fetch("api")
+      .then((res) => res.json())
+      .then((res) => {
+        console.log("res............", res);
+        return res;
+      });
+  }
 
   // 初始化和数据变更都执行
   useEffect(() => {
@@ -10,21 +19,27 @@ function UseEffect (props) {
   // 初始化执行
   // 如果数组[]里有值，若对应的值变更，则相应执行
   useEffect(() => {
+    (async () => {
+      let result = await loadData();
+      console.log("result..................", result);
+      setData((data) => ({ ...data, name: result.name }))
+    })();
     console.log("componentDidMount");
   }, []);
   useEffect(() => {
     return () => {
       console.log("componentUnMount");
-    }
+    };
   }, []);
   return (
     <>
       <div>{data.count}</div>
-      <button onClick={() => setData(() => ({ count: data.count + 1 }))}>
+      <button onClick={() => setData((data) => ({ ...data, count: data.count + 1 }))}>
         click
       </button>
+      <div>{data.name}</div>
     </>
   );
 }
 
-export default UseEffect
+export default UseEffect;
